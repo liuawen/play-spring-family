35dafc0f0c76a576e76b78bebeb1607b59957e46
Liu Awen <157514367@qq.com>
2020-03-22 09:16:31 +0800

    acquaintance spring.

---
+++ 1-Acquaintance-Spring/helloworld/src/main/java/com/awen/helloworld/HelloWorldApplication.java
@@ -0,0 +1,23 @@
+package com.awen.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+
+}
---
+++ 1-Acquaintance-Spring/src/test/java/com/awen/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awen.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
